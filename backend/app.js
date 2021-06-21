852a563a9bf624e092df883efa717689c31d330f
DESKTOP-HK4VH6P\dla67 <byeonggoon>
2021-06-21 19:06:20 +0900

    21.6.21 19시 야간학습

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+//나는 주석을 추가한 것이여
 const jwtMiddleware = require("./lib/jwtMiddleware");
 
 const indexRouter = require('./routes/index');
